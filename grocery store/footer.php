cbc6e26c55253814d28b20e191d4815a643c18bc
Bao Anh <nguyenlebaoanh05@gmail.com>
2025-04-15 09:16:56 -0400

    Logo redirects to home page and admin just logs in through the login button like everyone else

---
+++ grocery store/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="wishlist.php"> <i class="fas fa-angle-right"></i> Wishlist</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> Register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +514-333-4455 </p>
+         <p> <i class="fas fa-phone"></i> +438-222-6789 </p>
+         <p> <i class="fas fa-envelope"></i> ecoharvest@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Montreal, H3H 2T2</p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> &copy; Copyright @ <?= date('Y'); ?> by <span>EcoHarvest</span> | All Rights Reserved! </p>
+
+</footer>
